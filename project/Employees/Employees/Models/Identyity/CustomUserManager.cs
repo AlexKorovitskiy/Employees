dab4e5ed2b7781b592ad6d93c3a7030b2af5b2aa
AlexKorovitskiy <alex.korovitskiy@gmail.com>
2017-12-08 00:05:46 +0300

    123

---
+++ project/Employees/Employees/Models/Identyity/CustomUserManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Employees.Models.Identyity
+{
+    public class CustomUserManager : UserManager<ApplicationUser>
+    {
+        public CustomUserManager(CustomUserStore store)
+            : base(store)
+        {
+            this.PasswordHasher = new CustomPasswordHasher();
+        }
+
+        public override Task<ApplicationUser> FindAsync(string userName, string password)
+        {
+            Task<ApplicationUser> taskInvoke = Task<ApplicationUser>.Factory.StartNew(() =>
+            {
+                PasswordVerificationResult result = this.PasswordHasher.VerifyHashedPassword(userName, password);
+                if (result == PasswordVerificationResult.SuccessRehashNeeded)
+                {
+                    return Store.FindByNameAsync(userName).Result;
+                }
+                return null;
+            });
+            return taskInvoke;
+        }
+    }
+}
---
+++ project/Employees/Employees/Models/User.cs
@@ -0,0 +1,121 @@
+﻿using Employees.Dispatcher;
+using Employees.Models.API;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Employees.Models
+{
+    public class User: IEntity
+    {
+        public int? Id { get; set; }
+
+        [Display(Name = "Логин")]
+        [Required(ErrorMessage ="Пожалуйста, введите свой логин")]
+        public string Login { get; set; }
+
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Пожалуйста, введите свое имя")]
+        public string Name { get; set; }
+
+        [DataType (DataType.Password)]
+        [Display(Name = "Пароль")]
+        [Required(ErrorMessage = "Пожалуйста, введите свой пароль")]
+        public string Password { get; set; }
+
+
+        /// <summary>
+        /// Создаем контекст для сохранения сущности
+        /// </summary>
+        /// <returns></returns>
+        public Context PrepareSave()
+        {
+            Context context = new Context();
+            context.ProcedureName = "SaveUser";
+            context.Params.Add("Id", Id);
+            context.Params.Add("Name", Name);
+            context.Params.Add("Login", Login);
+            context.Params.Add("Password", Password);
+            return context;
+        }
+
+        /// <summary>
+        /// Создаем контекст для удаления сущности
+        /// </summary>
+        /// <returns></returns>
+        public Context PrepareDelete()
+        {
+            Context context = new Context();
+            context.ProcedureName = "DeleteUser";
+            context.Params.Add("Id", Id);
+            return context;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        #region Static
+
+        /// <summary>
+        /// Ззаполняем контекст своими полями для загрузки единственной сущности
+        /// </summary>
+        /// <param name="idCompany"></param>
+        /// <returns></returns>
+        public static Context PrepareLoad(int id)
+        {
+            Context context = new Context();
+            context.ProcedureName = "LoadUser";
+            context.Params.Add("Id", id);
+            return context;
+        }
+
+        /// <summary>
+        /// Ззаполняем контекст для загрузки всех сущностей
+        /// </summary>
+        /// <returns></returns>
+        public static Context PrepareLoadList()
+        {
+            Context context = new Context();
+            context.ProcedureName = "LoadUsers";
+            return context;
+        }
+
+        /// <summary>
+        /// Парсим данные из резалта и возвращаем экземпляр нашего класса
+        /// </summary>
+        /// <param name="contextResult"></param>
+        /// <returns></returns>
+        public static User ParseLoad(ContextResult contextResult)
+        {
+            User entity = new User();
+            entity.Id = (int)contextResult.Params["Id"];
+            entity.Name = contextResult.Params["Name"].ToString();
+            entity.Password = contextResult.Params["Password"].ToString();
+            entity.Login = contextResult.Params["Login"].ToString();
+            return entity;
+        }
+
+        /// <summary>
+        /// Парсим  из резалта список наших сущностей и возвращаем их коллекцию
+        /// </summary>
+        /// <param name="contextResultCollection"></param>
+        /// <returns></returns>
+        public static List<User> ParseLoadList(List<ContextResult> contextResultCollection)
+        {
+            List<User> collection = new List<User>();
+            foreach (var contextResult in contextResultCollection)
+            {
+                collection.Add(ParseLoad(contextResult));
+            }
+            return collection;
+        }
+
+        #endregion
+
+    }
+}
